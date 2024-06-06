2fed8611edbfd3473ae89542d04be4485ff029ff
daniel-budai <danielbudai@tutanota.com>
2024-06-06 12:36:55 +0200

    update 2

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,35 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("collect").addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabs[0].id },
-          files: ["src/content/content.js"],
-        },
-        () => {
-          chrome.tabs.sendMessage(
-            tabs[0].id,
-            { action: "SCRAPE_LINKS" },
-            (response) => {
-              const links = response.links;
-              const div = document.getElementById("links");
-
-              links.forEach((link) => {
-                const p = document.createElement("p");
-
-                if (link.startsWith("https://www.flashback.org/leave.php?u=")) {
-                  const actualUrl = new URL(link).searchParams.get("u");
-                  p.textContent = actualUrl;
-                } else {
-                  p.textContent = link;
-                }
-
-                div.appendChild(p);
-              });
-            }
-          );
-        }
-      );
+      scrapePage(tabs[0].id, 1);
     });
   });
 });
+
+function scrapePage(tabId, pageNumber) {
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tabId },
+      files: ["src/content/content.js"],
+    },
+    () => {
+      chrome.tabs.sendMessage(tabId, { action: "SCRAPE_LINKS" }, (response) => {
+        const links = response.links;
+        const div = document.getElementById("links");
+
+        links.forEach((link) => {
+          const a = document.createElement("a");
+          let actualUrl;
+          if (link.startsWith("https://www.flashback.org/leave.php?u=")) {
+            actualUrl = new URL(link).searchParams.get("u");
+          } else {
+            actualUrl = link;
+          }
+          a.textContent = actualUrl;
+          a.href = actualUrl;
+          a.target = "_blank";
+          div.appendChild(a);
+          div.appendChild(document.createElement("br"));
+        });
+
+        const nextPageNumber = pageNumber + 1;
+        const nextPageUrl = `https://www.flashback.org/t3610716p${nextPageNumber}`;
+        chrome.tabs.update(tabId, { url: nextPageUrl }, () => {
+          chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+            if (info.status === "complete") {
+              chrome.tabs.onUpdated.removeListener(listener);
+
+              setTimeout(() => {
+                scrapePage(tabId, nextPageNumber);
+              }, 5000);
+            }
+          });
+        });
+      });
+    }
+  );
+}
